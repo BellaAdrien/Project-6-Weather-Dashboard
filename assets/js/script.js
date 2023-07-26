f7848772888932c44f643adc38c693622d4d49c6
BellaAdrien <Iadrien2020@fau.edu>
2023-07-26 18:23:43 -0400

    save project 6

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+var apiKey="4d6b36f02b56cbd7f326765e11f1effe"
+var tempEl=document.getElementById("temp")
+var titleEl=document.getElementById("title")
+var windEl=document.getElementById("wind")
+var humidityEl=document.getElementById("humidity")
+var searchBtn=document.getElementById("search-btn")
+var cityInput=document.getElementById("city-input")
+
+
